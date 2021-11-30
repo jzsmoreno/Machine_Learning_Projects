5e72ec6bc1f53c9a81b87fecb43844bbf2fe22f1
unknown <equilibrio.top15@gmail.com>
2021-11-30 14:35:51 -0600

    added bar plots code

--- Titanic_MachineLearning_from_Disaster/dataAnalysis.py
+++ Titanic_MachineLearning_from_Disaster/dataAnalysis.py
@@ -17,10 +17,13 @@ import pandas as pd
 from pandas.plotting import radviz
 import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
+from dataPreparation import to_save_or_load
 
-dataPath = "./data/"
+#dataPath = "./data/"
+dataPath = "C:/Users/ivan_/Desktop/UDEMY/GitHub/Machine_Learning_Projects/Titanic_MachineLearning_from_Disaster/data/"
 
-def RadViz(data, features, target, title):
+
+def RadViz(data, features, target, title,save=False):
     """ This function is intended to perform RadViz analysis.
     This function is intended to perform RadViz analysis.
     The function returns a RadViz plot.
@@ -36,11 +39,12 @@ def RadViz(data, features, target, title):
     fig, ax = plt.subplots(figsize=(10, 10))
     radviz(data[features], target, ax=ax, colormap="viridis")
     plt.title(title+' for '+str(len(features))+' features '+'('+target+')')
-    fig.savefig('./reports/RadViz.png', dpi = 300)
+    if save:
+        fig.savefig('./reports/RadViz_for_{}_features.png'.format(len(features)), dpi = 300)
     plt.show()
 
 # Dimensionality Reduction with PCA
-def make_pca(data, features, target, title):
+def make_pca(data, features, target, title,save=False):
     """ This function is intended to perform PCA analysis.
     This function is intended to perform PCA analysis.
     The function returns a PCA plot.
@@ -60,14 +64,81 @@ def make_pca(data, features, target, title):
     plt.xlabel('First principal component')
     plt.ylabel('Second principal component')
     plt.title(title+' for '+str(len(features))+' features '+'('+target+')')
-    plt.savefig('./reports/PCA.png', dpi = 300)
+    if save:
+        plt.savefig('./reports/PCA_for_{}_features.png'.format(len(features)), dpi = 300)
     plt.show()
+    
+    
+def pie_forFeatures(survived_description_mean,axes,features,save=False):
+    
+    survived = ["No Survived","Survived"]
+    
+    axes[0,0].set_title("{}".format(features[0]))
+    axes[0,0].bar(survived,survived_description_mean.iloc[:,0])
+    
+    axes[0,1].set_title("{}".format(features[1]))
+    axes[0,1].bar(survived,survived_description_mean.iloc[:,1])
+    
+    axes[1,0].set_title("{}".format(features[2]))
+    axes[1,0].bar(survived,survived_description_mean.iloc[:,2])
+    
+    axes[1,1].set_title("{}".format(features[3]))
+    axes[1,1].bar(survived,survived_description_mean.iloc[:,3])
+    
+    axes[2,0].set_title("{}".format(features[4]))
+    axes[2,0].bar(survived,survived_description_mean.iloc[:,4])
+    
+    axes[2,1].set_title("{}".format(features[5]))
+    axes[2,1].bar(survived,survived_description_mean.iloc[:,5])
+    
+    axes[3,0].set_title("{}".format(features[6]))
+    axes[3,0].bar(survived,survived_description_mean.iloc[:,6])
+
+    axes[3,1].set_title("{}".format(features[7]))
+    axes[3,1].bar(survived,survived_description_mean.iloc[:,0])
+    
+
+    
+    if save:
+        plt.savefig('./reports/pie_for_{}_features.png'.format(len(features)), dpi = 300)
+        
+
 
 if __name__ == "__main__":
-    data = pd.read_csv(dataPath + "train_syntetic.csv")
+    #data = pd.read_csv(dataPath + "train_syntetic.csv")
+    # Lets load the data
+    data = to_save_or_load(None,dataPath+"data_frame.db",save=False) 
+    
+    
     RadViz(data, features = ["Survived", "Pclass", "Sex"
-, "Age", "SibSp", "Parch", "Fare", "Embarked", "Title_Name", "Ticket-label"]
-, target = "Survived", title = "RadViz")
+                             , "Age", "SibSp", "Parch", "Fare", "Embarked", "Title_Name", "Ticket-label"]
+           , target = "Survived", title = "RadViz")
     make_pca(data, features=["Survived", "Pclass", "Sex"
-, "Age", "SibSp", "Parch", "Fare", "Embarked", "Title_Name", "Ticket-label"]
-, target = "Survived", title = "PCA analysis")
+                             , "Age", "SibSp", "Parch", "Fare", "Embarked", "Title_Name", "Ticket-label"]
+             , target = "Survived", title = "PCA analysis")
+    
+    
+    # let´s get a description of survived
+    features = ["Survived", "Pclass", "Sex"
+                             , "Age", "SibSp", "Parch", "Fare", "Embarked", "Title_Name"]
+    survived_description = data[features].groupby("Survived").agg(["mean","max","min"])
+    # let´s only take mean 
+    survived_description_mean = data[features].groupby("Survived").agg(["mean"])
+    
+    # let´s do a pie graph 
+    fig, axes = plt.subplots(4,2, figsize=(10,12), sharex=True, sharey=False)
+    fig.suptitle('Mean of each feature with respect to survived')
+    pie_forFeatures(survived_description_mean,axes,features[1:],save=True)
+    
+    
+    """ Some useful insights about the pie graphs
+            Survived ---> Pclass =~ 1.5, sex =~ 0.2, age =~ 25, SibSp =~ 0.4,
+                            Parch =~ 0.4, Fare =~ 50, embarked =~ 1.5, Title_name =~ 1.75
+        Therefore, who survived in average are from the first and second class, woman, 
+        25 years old, with almost not family.
+    """
+
+    
+    
+    
+    
